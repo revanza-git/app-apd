b09b1f2ab1f036534bf49c176fac3fd5aab18ed3
revanza <revanza.raytama@gmail.com>
2020-09-08 11:32:19 +0700

    tes

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,16 +2,8 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import "./App.css";
 import configureStore from "../Store";
-import RegistrationPage from "./PageContainer/RegistrationPage";
-import PaymentPage from "./PageContainer/PaymentPage";
-import FinishedPage from "./PageContainer/FinishedPage";
-import LandingPage from "./PageContainer/LandingPage";
-import ConfirmationPage from "./PageContainer/ConfirmationPage";
-import ActivationPage from "./PageContainer/ActivationPage";
-import LoginPage from "./PageContainer/LoginPage";
-import DashboardPage from "./PageContainer/DashboardPage";
 import RoutingPage from "./PageContainer/RoutingPage";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const store = configureStore();
 
@@ -20,12 +12,6 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Router>
-          {/* <Route exact path={process.env.PUBLIC_URL}>
-            <Redirect to={process.env.PUBLIC_URL + "/welcome"} />
-          </Route>
-          <Route exact path="/">
-            <Redirect to={process.env.PUBLIC_URL + "/welcome"} />
-          </Route> */}
           <Route path="/" component={RoutingPage} />
         </Router>
       </Provider>
